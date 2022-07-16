5ba9b572edd6da5cfb1612801e2f1c5390f352a0
clay <clay.nichols@gmail.com>
2022-07-15 23:51:37 -0500

    Test new approach with object as entire calc

--- scripts.js
+++ scripts.js
@@ -1,15 +1,45 @@
 const display = document.querySelector("#displayContainer");
 let buttons = document.querySelectorAll(".button");
-
 Array
     .from(buttons)
     .forEach(addEvent)    
 
 
 
+const calc = {
+    num1 : 0,
+    num2 : 0,
+    // add: function(num1, num2) {
+    //     return num1 + num2;
+    // },
+    // subtract: function(num1, num2) {
+    //     return num1 - num2;
+    // },
+    // multiply: function(num1, num2) {
+    //     return num1 * num2;
+    // },
+    // divide: function(num1, num2) {
+    //     return num1 / num2;
+    // }, 
+    operateAdd: function(num1, num2) {
+        return add(num1, num2);
+    },
+    operateSubtract: function() {
+        return subtract(num1, num2);
+    }, 
+    operateMultiply: function() {
+        return multiply(num1, num2);
+    }, 
+    operateDivide: function() {
+        return divide(num1, num2);
+    },  
+};
+
+
 function addEvent(element) {
     element.addEventListener("click", (e) => {
         let displayText = e.target.value;
+        calc.num1 = e.target.value;
         display.innerHTML = displayText;
 })};
 
@@ -30,24 +60,23 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
-function operate(x, op, y) {
-    switch (op) {
-        case '+':
-            console.log(add(x, y));
-            break;
-        case '-':
-            console.log(subtract(x, y));
-            break;
-        case '*':
-            console.log(multiply(x, y));
-            break;
-        case '/':
-            console.log(divide(x, y));
-            break;
-    }
-}
-// let num1 = 3;
-// let num2 = 5;
+// function operate(x, op, y) {
+//     switch (op) {
+//         case '+':
+//             console.log(add(x, y));
+//             break;
+//         case '-':
+//             console.log(subtract(x, y));
+//             break;
+//         case '*':
+//             console.log(multiply(x, y));
+//             break;
+//         case '/':
+//             console.log(divide(x, y));
+//             break;
+//     }
+// }
+
 // let opInput = "*";
 
 // operate(num1, opInput, num2);
