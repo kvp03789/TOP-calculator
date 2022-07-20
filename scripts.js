59d8f12eedc742322bc13c3795478e5d4927d9dd
clay <clay.nichols@gmail.com>
2022-07-20 16:19:46 -0500

    Fix some errors with equal button and operation buttons

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ Array
     .forEach(addSymbolEvent)
 
 const calc = {
-
+    switch: true,
     num1: 0,
     num2: 0,
     displayArray : [],
@@ -29,11 +29,14 @@ const calc = {
     displayString: '',
     answer: '',
     operateAdd: function(num1, num2) {
+        if (this.displayArray.length === 0) {}
+        else {
         num1 = parseInt(this.displayArray[0]);
         num2 = parseInt(this.displayArray[2]);
         this.answer = (num1 + num2);
         
         console.log(this.answer);
+        }
     },
     operateSubtract: function(num1, num2) {
         num1 = parseInt(this.displayArray[0]);
@@ -52,6 +55,7 @@ const calc = {
     operateDivide: function(num1, num2) {
         num1 = parseInt(this.displayArray[0]);
         num2 = parseInt(this.displayArray[2]);
+        if (num2 === 0) {return alert("u can't divide by 0! >.<")}
         this.answer = (num1 / num2);
         
         console.log(this.answer);
@@ -69,6 +73,7 @@ const calc = {
         this.currentNumber.length = 0;
         this.joined = '';
         this.displayString = '';
+        this.answer = 0;
         display.innerHTML = '----';
         displayOne.innerHTML = '----';
         displayString.innerHTML = '----';
@@ -76,40 +81,45 @@ const calc = {
     getAnswer: function () {
         if (this.displayArray[1] === '/') {
             this.operateDivide();
-            
+            this.displayArray[0] = this.answer;
+            this.displayArray.length = 1;
         }
         if (this.displayArray[1] ==='*') {
             this.operateMultiply();
-            
+            this.displayArray[0] = this.answer;
+            this.displayArray.length = 1;
         }
         if (this.displayArray[1] === '-') {  
             this.operateSubtract();
-            
+            this.displayArray[0] = this.answer;
+            this.displayArray.length = 1;
         }
         if (this.displayArray[1] === '+') {
             this.operateAdd();
+            this.displayArray[0] = this.answer;
+            this.displayArray.length = 1;
             
         }
-        }
+
+        }      
 };
 
 //ADD EVENT TO NUM BUTTONS
 function addEvent(element) {
     element.addEventListener("click", (e) => {
-   
+    if (calc.switch === true) {
         let value = e.target.value;
         calc.currentNumber.push(value);
         calc.joined = calc.currentNumber.join(''); 
         displayOne.innerHTML = calc.joined;
-        
+    }
+    else {}
 })};
 
 //ADD EVENT TO SYMBOL BUTTONS
 function addSymbolEvent(element) {
     element.addEventListener("click", (e) => {
-        // if (calc.displayArray.slice(-1) == '/' || calc.displayArray.slice(-1) == '*' || 
-        // calc.displayArray.slice(-1) == '-' || calc.displayArray.slice(-1) == '+') {}
-        // else {
+        if (calc.switch === true) {    
         if (calc.displayArray.length === 1) {
             calc.currentNumber.length = 0;
             calc.joined = '';
@@ -121,22 +131,26 @@ function addSymbolEvent(element) {
         else {
         calc.displayArray.push(calc.joined);
         calc.numbersArray.push(calc.joined);
+        calc.getAnswer();
         calc.currentNumber.length = 0;
         calc.joined = '';
         calc.displayArray.push(e.target.value);
         calc.symbolsArray.push(e.target.value);
         display.innerHTML = calc.displayArray;
         displayOne.innerHTML = '----';
+        
         }
+        
+        displayOne.innerHTML = `${calc.answer}`;
+        } 
+        else {};   
     })
 }
 
 //ADD DISPLAY STRING EVENT TO EVERY BUTTON, WITH SPACE IF SYMBOL BTN
 function recordToString(element) {
     element.addEventListener("click", (e) => {
-        // if (calc.displayArray.slice(-1) == '/' || calc.displayArray.slice(-1) == '*' || 
-        // calc.displayArray.slice(-1) == '-' || calc.displayArray.slice(-1) == '+') {}
-        // else {
+    if (calc.switch === true) { 
         if (e.target.value === '+' || e.target.value === '-' || e.target.value === '*' || e.target.value === '/' || e.target.value === '=' ) {
             calc.displayString = calc.displayString.concat(' ' + e.target.value.toString() + ' ');
             displayString.innerHTML = calc.displayString;
@@ -145,13 +159,16 @@ function recordToString(element) {
         calc.displayString = calc.displayString.concat(e.target.value.toString());
         displayString.innerHTML = calc.displayString;
         }
+    }
+    else {};    
     })
 }
 
 //ERASE EVERYTHING EVENT
 const clearEverythingButton = document.querySelector("#clearEverythingButton");
 clearEverythingButton.addEventListener("click", () => {
-    calc.clearEverything();   
+    calc.clearEverything();  
+    calc.switch = true; 
 });
 
 // ERASE ONLY LAST CHAR EVENT
@@ -163,6 +180,7 @@ clearOneButton.addEventListener("click", () => {
 //EQUAL BUTTON EVENT
 const equalButton = document.querySelector("#eqButton");
 equalButton.addEventListener("click", () => {
+    if (calc.switch === true) { 
     calc.displayArray.push(calc.joined);
     calc.getAnswer();
     calc.displayArray.length = 0; 
@@ -171,7 +189,9 @@ equalButton.addEventListener("click", () => {
     calc.joined = '';    
     display.innerHTML = '----';
     displayOne.innerHTML = `${calc.answer}`;
-        
+    calc.switch = false;
+    }
+    else {};     
 });
 
 
